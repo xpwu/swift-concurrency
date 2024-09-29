a14450e272b2a2d753f7fb08d64ab0471fc9a00e
xpwu <diodexp@foxmail.com>
2024-09-29 19:32:18 +0800

    add channel.close()

--- Sources/xpwu_concurrency/channel.swift
+++ Sources/xpwu_concurrency/channel.swift
@@ -5,57 +5,112 @@
 
 import Foundation
 
+public struct ChannelClosed: Error {
+	public var msg: String = ""
+}
+
 public protocol SendChannel<E> {
 	associatedtype E: Sendable
-	func Send(_ e: E) async
+	func SendOrFailed(_ e: E) async ->ChannelClosed?
+	func Close(reason: String) async
 }
 
 public extension SendChannel {
 	func Send() async where E == Void {
 		await Send(())
 	}
+	
+	func Send(_ e: E) async {
+		_ = await SendOrFailed(e)
+	}
+	
+	func Close() async {
+		await Close(reason: "")
+	}
 }
 
 public protocol ReceiveChannel<E> {
 	associatedtype E: Sendable
-	func Receive() async -> E
+	func ReceiveOrFailed() async -> Result<E, ChannelClosed>
+}
+
+public extension ReceiveChannel {
+	func Receive() async -> E? {
+		switch await ReceiveOrFailed() {
+		case .success(let r):
+			return r
+		case .failure(let err):
+			return nil
+		}
+	}
 }
 
 actor channel<E: Sendable> {
 	let data: queue<E> = queue()
-	let sendSuspend: queue<()->Void> = queue()
-	let receiveSuspend: queue<()->Void> = queue()
+	let sendSuspend: queue<(ChannelClosed?)->Void> = queue()
+	let receiveSuspend: queue<(Result<E, ChannelClosed>)->Void> = queue()
 	let max: Int
+	var closed: ChannelClosed?
 	
 	init(_ max: Int = 0) {
 		self.max = max
 	}
 	
-	func send(_ e:E, waiting: @escaping @Sendable ()->Void) ->(todo: (()->Void)?, needWait: Bool) {
-		data.en(e)
-		let todo = receiveSuspend.de()
-		var needWait = false
-		
-		if data.count >= max {
-			sendSuspend.en(waiting)
-			needWait = true
+	func close(reason msg: String = "") {
+		closed = ChannelClosed(msg: msg)
+		while let s = sendSuspend.de() {
+			s(closed)
 		}
-		
-		return (todo, needWait)
+		while let r = receiveSuspend.de() {
+			r(.failure(closed!))
+		}
 	}
 	
-	// return if value == nil {need wait} else {not need wait}
-	func receive(waiting: @escaping @Sendable (E)->Void) async ->(todo: (()->Void)?, value: E?) {
-		let value = data.de()
-		let todo = sendSuspend.de()
+	enum isSuspended<T> {
+		case Yes, No(todo: (()->Void)?, value: T), Failed(ChannelClosed)
+	}
+	
+	func send(_ e:E, ifsuspend waiting: @escaping @Sendable (ChannelClosed?)->Void) -> isSuspended<Void> {
 		
-		if value == nil {
-			receiveSuspend.en {[unowned self] in
-				waiting(self.data.de()!)
-			}
+		if let closed {
+			return .Failed(closed)
 		}
 		
-		return (todo, value)
+		data.en(e)
+		if data.count >= max {
+			sendSuspend.en(waiting)
+			return .Yes
+		}
+		
+		let rfun = receiveSuspend.de()
+		var todo: (()->Void)? = nil
+		if let rfun {
+			let d = data.de()!
+			todo = {rfun(.success(d))}
+		}
+		
+		return .No(todo: todo, value: ())
+	}
+	
+	func receive(ifsuspend waiting: @escaping @Sendable (Result<E, ChannelClosed>)->Void) -> isSuspended<E> {
+		
+		if let closed {
+			return .Failed(closed)
+		}
+		
+		let value = data.de()
+		guard let value else {
+			receiveSuspend.en(waiting)
+			return .Yes
+		}
+		
+		let sfun = sendSuspend.de()
+		var todo: (()->Void)? = nil
+		if let sfun {
+			todo = {sfun(nil)}
+		}
+		
+		return .No(todo: todo, value: value)
 	}
 
 }
@@ -73,18 +128,25 @@ extension Int {
 }
 
 extension Channel: SendChannel {
+	public func Close(reason: String) async {
+		await chan.close(reason: reason)
+	}
 	
-	public func Send(_ e: E) async {
-		await withCheckedContinuation({ (continuation: CheckedContinuation<Void, Never>) in
+	public func SendOrFailed(_ e: E) async -> ChannelClosed? {
+		return await withCheckedContinuation({ (continuation: CheckedContinuation<ChannelClosed?, Never>) in
 			Task {
-				let (todo, needWait) = await chan.send(e) {
-					continuation.resume()
+				let isSuspended = await self.chan.send(e) {err in
+					continuation.resume(returning: err)
 				}
 				
-				todo?()
-				
-				if !needWait {
-					continuation.resume()
+				switch isSuspended {
+				case .Failed(let err):
+					continuation.resume(returning: err)
+				case .No(let todo , _):
+					todo?()
+					continuation.resume(returning: nil)
+				case .Yes:
+					break
 				}
 			}
 		})
@@ -92,17 +154,21 @@ extension Channel: SendChannel {
 }
 
 extension Channel: ReceiveChannel {
-	public func Receive() async -> E {
-		await withCheckedContinuation({ (continuation: CheckedContinuation<E, Never>) in
+	public func ReceiveOrFailed() async -> Result<E, ChannelClosed> {
+		return await withCheckedContinuation({ (continuation: CheckedContinuation<Result<E, ChannelClosed>, Never>) in
 			Task {
-				let (todo, value) = await chan.receive {value in
+				let isSuspended = await self.chan.receive {value in
 					continuation.resume(returning: value)
 				}
 				
-				todo?()
-				
-				if let v = value {
-					continuation.resume(returning: v)
+				switch isSuspended {
+				case .Failed(let err):
+					continuation.resume(returning: .failure(err))
+				case let .No(todo, value):
+					todo?()
+					continuation.resume(returning: .success(value))
+				case .Yes:
+					break
 				}
 			}
 		})
