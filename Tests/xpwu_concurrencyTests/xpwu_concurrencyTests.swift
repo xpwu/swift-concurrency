55cd6606b5346319c28e661c26ad573365d7f194
xpwu <diodexp@foxmail.com>
2024-10-01 02:08:14 +0800

    add test

--- Tests/xpwu_concurrencyTests/xpwu_concurrencyTests.swift
+++ Tests/xpwu_concurrencyTests/xpwu_concurrencyTests.swift
@@ -9,4 +9,20 @@ final class xpwu_concurrencyTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+	
+	func testChannel() async throws {
+		let ch = Channel<Bool>(buffer: 1)
+		await ch.Send(true)
+		let r = await ch.Receive()
+		XCTAssertNotNil(r)
+		XCTAssertTrue(r!)
+	}
+	
+	func testChannel2() async throws {
+		let ch = Channel<Bool>(buffer: 1)
+		async let _ = ch.Send(true)
+		let r = await ch.Receive()
+		XCTAssertNotNil(r)
+		XCTAssertTrue(r!)
+	}
 }
