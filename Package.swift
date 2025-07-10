a3ea56c8cb75ec9a6996e3f5bb856c850a542c12
xpwu <diodexp@foxmail.com>
2025-07-10 19:00:40 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,9 @@ let package = Package(
             name: "xpwu_concurrency",
             targets: ["xpwu_concurrency"]),
     ],
-		dependencies: [.package(url: "https://github.com/xpwu/swift-x", .upToNextMajor(from: "1.0.0")),],
+		dependencies: [
+			.package(url: "https://github.com/xpwu/swift-x", .upToNextMajor(from: "1.0.3")),
+		],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
