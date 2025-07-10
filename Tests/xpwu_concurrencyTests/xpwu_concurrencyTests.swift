bb7d8331f1285c5627334ced6839f4e6971b3bc6
xpwu <diodexp@foxmail.com>
2025-07-10 19:00:43 +0800

    Update xpwu_concurrencyTests.swift

--- Tests/xpwu_concurrencyTests/xpwu_concurrencyTests.swift
+++ Tests/xpwu_concurrencyTests/xpwu_concurrencyTests.swift
@@ -87,7 +87,7 @@ final class xpwu_concurrencyTests: XCTestCase {
 		let sem = Semaphore(permits: 3)
 		
 		async let group = withTaskGroup(of: Void.self, returning: Bool.self) { group in
-			for _ in 0 ... con {
+			for _ in 1...con {
 				group.addTask {
 					let err = await sem.AcquireOrErr()
 					XCTAssertNil(err)
@@ -98,14 +98,55 @@ final class xpwu_concurrencyTests: XCTestCase {
 			return true
 		}
 		
-		try! await Task.sleep(nanoseconds: 1000000000)
+		try! await Task.sleep(nanoseconds: 3000000000)
 		
-		for _ in 0 ... con {
+		for _ in 1...con {
 			await sem.Release()
 		}
 		
 		let r = await group
 		XCTAssertTrue(r)
+	}
+	
+	func testSem3() async throws {
+		let con = 9
+		let sem = Semaphore(permits: 3)
+		
+		async let group = withTaskGroup(of: Void.self, returning: Bool.self) { group in
+			for _ in 1...con {
+				group.addTask {
+					let err = await sem.AcquireOrErr()
+					XCTAssertNil(err)
+				}
+			}
+			
+			await group.waitForAll()
+			return true
+		}
+		
+		try! await Task.sleep(nanoseconds: 3000000000)
+		
+		for _ in 1...con/3 + 1 {
+			await sem.Release(3)
+		}
+		
+		let r = await group
+		XCTAssertTrue(r)
+	}
+	
+	func testSemRelease() async throws {
+		let sem = Semaphore(permits: 5)
+		await sem.Release()
+		await sem.Release()
+		let ava = await sem.AvailablePermits
+		XCTAssertEqual(sem.Permits, ava)
+	}
+	
+	func testSemRelease3() async throws {
+		let sem = Semaphore(permits: 5)
+		await sem.Release(3)
+		let ava = await sem.AvailablePermits
+		XCTAssertEqual(sem.Permits, ava)
 	}
 	
 	func testSemCancel() async throws {
